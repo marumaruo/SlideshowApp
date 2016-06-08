ce6ccf37a8f8d5a496d57a80d35d9b87adf0980e
bc0067042 <bc0067042@ishi-pc20192992.local>
2016-06-08 18:50:05 +0900

    ページ処理を追加

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,16 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    //変数定義
+    var timer : NSTimer!
+    var image: [UIImage] = []
+    var imageView: [UIImageView?] = []
+    var currentPage = 0
+    var maxPage = 5
+    var playing = false
+    
+    //IB接続
     @IBOutlet weak var uiImageView: UIImageView!
+    
+    @IBOutlet weak var playButton: UIButton!
     
     @IBAction func unwind(segue: UIStoryboardSegue){
     }
     
-    var timer : NSTimer!
-
+    @IBAction func prev(sender: AnyObject) {
+        if currentPage != 0 {currentPage = currentPage - 1
+        } else {currentPage = maxPage}
+        
+        uiImageView.image = image[currentPage]
+    }
+    
+    @IBAction func next(sender: AnyObject) {
+        if currentPage != maxPage {currentPage = currentPage + 1
+        } else {currentPage = 0}
+        
+        uiImageView.image = image[currentPage]
+    }
+    
+    @IBAction func play(sender: AnyObject) {
+        if playing {
+            playButton.setTitle("再生", forState: UIControlState.Normal)
+            playing = false
+        } else {
+            playButton.setTitle("停止", forState: UIControlState.Normal)
+        playing = true}
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //画像の配列格納
+        image = [UIImage(named:"1.jpg")!,
+        UIImage(named:"2.gif")!,
+        UIImage(named:"3.jpg")!,
+        UIImage(named:"4.jpeg")!,
+        UIImage(named:"5.jpg")!,
+        UIImage(named:"6.png")!]
+        
+        //imageViewにimageを追加
+        uiImageView.image = image[currentPage]
+        
         
 //        uiImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "imageTapped"))
         uiImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.imageTapped)))
